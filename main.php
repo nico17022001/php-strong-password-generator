e37e2993293a4985e7018b7c19efdf2103dc0ce9
nico17022001 <nico17022001boolean@gmail.com>
2023-05-17 16:57:53 +0200

    cambiato submit con genera

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
     <label for="exampleInputEmail1">Inserisci il numero che desideri</label>
     <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="esempio 10" name="selectNumber" style="-moz-appearance: textfield">
   </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary">Genera</button>
   <div>
     <?php 
       if (isset($_GET["selectNumber"]) && $password = generatePassword($_GET["selectNumber"])){
